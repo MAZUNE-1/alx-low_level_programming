680c01d44c26564b68477926c741fe3ffae757a2
MAZUNE-1 <1995nasser@gmail.com>
2023-09-10 00:38:28 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,12 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
 /**
  * main - Entry point
  * Description: checks on the last digit of the assigned var
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	int n, m;
@@ -19,7 +20,7 @@ int main(void)
 	}
 	else if (m < 6 && m != 0)
 	{
-		printf("Last digit of %d is %d is less than 6 and not 0\n", n, m);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	}
 	else
 	{
@@ -27,3 +28,4 @@ int main(void)
 	}
 	return (0);
 }
+
