f4e06cbf2d3fecea3ff9c84145e13aae9e92a546
MAZUNE-1 <1995nasser@gmail.com>
2023-09-12 11:19:10 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_aphabet_x10 - Make alphabet x10 times
+ * print_alphabet_x10 - Make alphabet x10 times
  * Return: void
  */
 
@@ -20,5 +20,6 @@ _putchar(ch);
 _putchar('\n');
 i++;
 }
+
 
 }
