e898ba62fa08985bfd220525986b92f69a392c2d
MAZUNE-1 <1995nasser@gmail.com>
2023-09-13 06:20:10 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,26 +1,27 @@
 #include "main.h"
 
 /**
- * print_sign - Printys sum
- * @n: The nmber
- * Return: 1 for all
+ * print_sign - prints sign of a number
+ * @n: function parameter
+ * Return: 1,0 or -1
  */
 
 int print_sign(int n)
 {
-if (n > 0)
-{
-_putchar(43);
-return (1);
-}
-else if (n < 0)
-{
-_putchar(45);
-return (-1);
-}
-else
-{
-_putchar(48);
-return (0);
+	if (n > 0)
+	{
+		_putchar(',');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 
 }
