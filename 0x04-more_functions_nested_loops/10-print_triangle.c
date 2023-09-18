baf05029dacff2b23cadd49ab30472ee5efa3c60
MAZUNE-1 <1995nasser@gmail.com>
2023-09-18 08:19:00 -0700

    10-triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,32 @@
 #include "main.h"
 /**
- * print_traingle - print a triangle
- * @size: the size of the traingle
+ * print_triangle - prints a triangle, followed by a new line
+ * @size: size of the triangle
  */
-void print_traingle(int size)
+void print_triangle(int size)
 {
-	int i, j;
-
-	if (size > 0)
+	if (size <= 0)
 	{
-	for (i = 0; i < size; i++)
-	{
-	for (j = 0; j < size; j++)
-	{
-	if (j < size - (i + 1))
-	{
-		_putchar(' ');
+	_putchar('\n');
 	}
 	else
 	{
-		_putchar('#');
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+	for (j = i; j < size; j++)
+	{
+	_putchar(' ');
 	}
+
+	for (j = 1; j <= i; j++)
+	{
+	_putchar('#');
 	}
+
 	_putchar('\n');
 	}
 	}
-	else
-	{
-		_putchar('\n');
-	}
 }
+
