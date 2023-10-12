7a44cf599de1a8bfaddddd5fbc9a6ac1fdbf242c
MAZUNE-1 <1995nasser@gmail.com>
2023-10-12 01:10:36 -0700

    static

--- 0x09-static_libraries/3-strcmp.c
+++ 0x09-static_libraries/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcmp - compare string values
  * @s1: input value
@@ -14,11 +13,11 @@ int _strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		if (s1[i] != s2[i]
-				{
-				return (s1[i] - s2[i]);
-				}
-				i++;
-				}
-				return (0);
-				}
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
+	}
+	return (0);
+}
